299c6d45a7a726e9e321d36d8f0d824dbbbc5517
Gabriel Messias <gabrielmessias@outlook.com>
2025-05-27 14:33:48 -0300

    adicionando comentários ao código

--- super_trunfo.c
+++ super_trunfo.c
@@ -2,10 +2,12 @@
 
 int main () {
 
+    //declaracao das variaveis
     char estado1, estado2, codigo1 [3], codigo2 [3], nomeDaCidade1 [50], nomeDaCidade2 [50];
     int populacao1, populacao2, pontosTuristicos1, pontosTuristicos2;
     float areaEmKm1, areaEmKm2, pib1, pib2;
 
+    //dados de entrada, usuario atribui valor as variaveis
     printf ("Digite uma letra de A a H para representar o estado da carta 1: \n");
     scanf (" %c", &estado1);
     printf ("Digite uma letra de A a H para representar o estado da carta 2: \n");
@@ -35,8 +37,8 @@ int main () {
     printf ("Digite o PIB da cidade da carta 2: \n");
     scanf ("%f", &pib2);
 
+    //dados de saida, impressao dos dados referentes as cartas
     printf ("Carta 1: \n Estado: %c\n Código: %c%s\n Nome: %s\n População: %d\n Pontos Turisticos: %d\n Area: %.2f\n PIB: %.2f\n", estado1, estado1, codigo1, nomeDaCidade1, populacao1, pontosTuristicos1, areaEmKm1, pib1);
-
     printf ("Carta 2: \n Estado: %c\n Código: %c%s\n Nome: %s\n População: %d\n Pontos Turisticos: %d\n Area: %.2f\n PIB: %.2f\n", estado2, estado2, codigo2, nomeDaCidade2, populacao2, pontosTuristicos2, areaEmKm2, pib2);
 
     return 0;
